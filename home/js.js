35682625488ffd8d9a37d34786d8444830aeb566
ExtosNex <ExtosNex@users.noreply.github.com>
2018-06-07 14:37:51 +1000

    Add files via upload

---
+++ home/js.js
@@ -0,0 +1,18 @@
+javascript:
+var input = prompt("this is a test");
+
+if(input === "/.,mnbvcxzaq12wsdfghjkl;'" ){
+    alert("it worked boi");
+} else {
+    alert("yeh nah... didn't work fam");
+}
+
+javascript:
+var chooseEmail = prompt("Enter Email Initials");
+var x = "null";
+var r = "null";
+if(chooseEmail === "1qawsedrftgyhujikolp;[']"){
+  alert("Email " + chooseEmail + " Accepted");
+} else {
+    alert("that's cool bro")
+}
