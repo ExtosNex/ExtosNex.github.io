dd33b42d76dd3f04d180deb7707201fb87450d68
ExtosNex <ExtosNex@users.noreply.github.com>
2018-09-08 22:40:37 +1000

    nav bar

---
+++ nice/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+      $("#poke1").mouseenter(function(){
+        window.setTimeout(p1,175);
+        function p1(){
+        $("#poke1").hide();
+        $ akuwdfawiygf 2").show();
+        $ akuwdfawiygf 3").hide();
+        } akuwdfawiygf 
+      }); akuwdfawiygf 
+      $("#poke2").mouseenter(function(){
+        window.setTimeout(p2,175);
+        function p2(){
+        $("#poke1").show();
+        $("#poke2").hide();
+        $("#poke3").show();
+        };
+      });
+      $("#poke3").mouseenter(function(){
+        window.setTimeout(p3,175);
+        function p3(){
+        $("#poke1").show();
+        $("#poke2").show();
+        $("#poke3").hide();
+       };
+      });
+    });
+     akuwdfawiygf 
+    $(document). akuwdfawiygf ready(function(){
+      $("#kachow akuwdfawiygf ").mouseenter(function(){
+        $("#mcqw akuwdfawiygf een").show();
+        window.setTimeout(mcqween,2000);
+        function mcqween(){
+        $("#mcqween").hide();
+        };
+      });
+    });
